7af934f4b0aa25b644f27635a31e9f085fc509d5
3noch <eacameron@gmail.com>
2017-04-18 03:10:46 -0400

    Use consistent indentation (2 spaces)

--- variables/StampVariable.php
+++ variables/StampVariable.php
@@ -3,25 +3,25 @@ namespace Craft;
 
 class StampVariable
 {
-	public function er($fileName, $mode='file')
-	{
+  public function er($fileName, $mode='file')
+  {
     $documentRoot = craft()->stamp->getSetting('stampPublicRoot')!=null ? craft()->stamp->getSetting('stampPublicRoot') : $_SERVER['DOCUMENT_ROOT'];
     $filePath = $documentRoot . $fileName;
-    
+
     if ($fileName != '' && file_exists($filePath)) {
       $path_parts = pathinfo($fileName);
-      
-			if ($mode=='file') {
-				return $path_parts['dirname'] . '/' . $path_parts['filename'] . '.' . filemtime($filePath) . '.' . $path_parts['extension'];
-			} else if ($mode=='folder') {
-				return $path_parts['dirname'] . '/' . filemtime($filePath) . '/' . $path_parts['filename'] . '.' . $path_parts['extension'];
-			} else if ($mode=='query') {
-				return $path_parts['dirname'] . '/' . $path_parts['filename'] . '.' . $path_parts['extension'] . '?ts=' . filemtime($filePath);
-			} else if ($mode=='tsonly') {
-				return filemtime($filePath);
-			}
+
+      if ($mode=='file') {
+        return $path_parts['dirname'] . '/' . $path_parts['filename'] . '.' . filemtime($filePath) . '.' . $path_parts['extension'];
+      } else if ($mode=='folder') {
+        return $path_parts['dirname'] . '/' . filemtime($filePath) . '/' . $path_parts['filename'] . '.' . $path_parts['extension'];
+      } else if ($mode=='query') {
+        return $path_parts['dirname'] . '/' . $path_parts['filename'] . '.' . $path_parts['extension'] . '?ts=' . filemtime($filePath);
+      } else if ($mode=='tsonly') {
+        return filemtime($filePath);
+      }
     } else {
       return '';
     }
-	}
+  }
 }
