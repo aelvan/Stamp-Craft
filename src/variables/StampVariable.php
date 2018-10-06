cce2e0e412e5c709ca4df9ba57c9bbdae77c8e2d
André Elvan <andre@vaersaagod.no>
2018-10-06 23:57:29 +0200

    Adds support for using crc32 hashing instead of timestamps.

--- src/variables/StampVariable.php
+++ src/variables/StampVariable.php
@@ -16,35 +16,37 @@ class StampVariable
 {
     /**
      * Main stamp template variable.
-     * 
-     * @param        $fileName
+     *
+     * @param string $fileName
      * @param string $mode
+     * @param string $alg
      *
      * @return string
      */
-    public function er($fileName, $mode = 'file'): string
+    public function er($fileName, $mode = 'file', $alg = 'ts'): string
     {
         $documentRoot = \Yii::getAlias(Stamp::getInstance()->getSettings()->publicRoot ?? '@webroot');
-        $filePath = $this->_removeDoubleSlashes($documentRoot.'/'.$fileName);
+        $filePath = $this->_removeDoubleSlashes($documentRoot . '/' . $fileName);
 
         if ($fileName !== '' && file_exists($filePath)) {
             $path_parts = pathinfo($fileName);
-            $tstamp = filemtime($filePath);
+
+            $stamp = $alg === 'hash' ? $this->num_hash_file($filePath) : filemtime($filePath);
 
             if ($mode === 'file') {
-                return $path_parts['dirname'].'/'.$path_parts['filename'].'.'.$tstamp.'.'.$path_parts['extension'];
+                return $path_parts['dirname'] . '/' . $path_parts['filename'] . '.' . $stamp . '.' . $path_parts['extension'];
             }
 
             if ($mode === 'folder') {
-                return $path_parts['dirname'].'/'.$tstamp.'/'.$path_parts['filename'].'.'.$path_parts['extension'];
+                return $path_parts['dirname'] . '/' . $stamp . '/' . $path_parts['filename'] . '.' . $path_parts['extension'];
             }
 
             if ($mode === 'query') {
-                return $path_parts['dirname'].'/'.$path_parts['filename'].'.'.$path_parts['extension'].'?ts='.$tstamp;
+                return $path_parts['dirname'] . '/' . $path_parts['filename'] . '.' . $path_parts['extension'] . '?ts=' . $stamp;
             }
 
             if ($mode === 'tsonly') {
-                return $tstamp;
+                return $stamp;
             }
         }
 
@@ -54,12 +56,23 @@ class StampVariable
     /**
      * Removes double slashes from string
      *
-     * @param $path
-     *
+     * @param string $path
      * @return mixed
      */
     private function _removeDoubleSlashes($path)
     {
         return preg_replace('#/+#', '/', $path);
+    }
+
+    /**
+     * Exactly like hash_file except that the result is always numeric
+     * (consisting of characters 0-9 only).
+     *
+     * @param string $filePath
+     * @return string
+     */
+    public function num_hash_file($filePath)
+    {
+        return implode(unpack('C*', hash_file('crc32b', $filePath, true)));
     }
 }
